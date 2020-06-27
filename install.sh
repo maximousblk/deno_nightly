720675d8804cab8fd3e73d0fc9dd3077e76e6cee
Maximous Black <maximousblk@gmail.com>
2020-06-27 21:03:50 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,59 @@
+set -e
+
+if [ "$(uname -m)" != "x86_64" ]; then
+	echo "Error: Unsupported architecture $(uname -m). Only x64 binaries are available." 1>&2
+	exit 1
+fi
+
+if ! command -v unzip >/dev/null; then
+	echo "Error: unzip is required to install Deno (see: https://github.com/denoland/deno_install#unzip-is-required)." 1>&2
+	exit 1
+fi
+
+case $(uname -s) in
+Darwin) target="x86_64-apple-darwin" ;;
+*) target="x86_64-unknown-linux-gnu" ;;
+esac
+
+if [ $# -eq 0 ]; then
+	deno_asset_path=$(
+		curl -sSf https://github.com/maximousblk/deno_nightly/releases |
+			grep -o "/denoland/deno/releases/download/.*/deno-${target}\\.zip" |
+			head -n 1
+	)
+	if [ ! "$deno_asset_path" ]; then
+		echo "Error: Unable to find latest Deno release on GitHub." 1>&2
+		exit 1
+	fi
+	deno_uri="https://github.com${deno_asset_path}"
+else
+	deno_uri="https://github.com/maximousblk/deno_nightly/releases/download/${1}/deno-${target}.zip"
+fi
+
+deno_install="${DENO_INSTALL:-$HOME/.deno}"
+bin_dir="$deno_install/bin"
+exe="$bin_dir/deno-nightly"
+
+if [ ! -d "$bin_dir" ]; then
+	mkdir -p "$bin_dir"
+fi
+
+curl --fail --location --progress-bar --output "$exe.zip" "$deno_uri"
+cd "$bin_dir"
+unzip -o "$exe.zip"
+chmod +x "$exe"
+rm "$exe.zip"
+
+echo "Deno was installed successfully to $exe"
+if command -v deno-nightly >/dev/null; then
+	echo "Run 'deno-nightly --help' to get started"
+else
+	case $SHELL in
+	/bin/zsh) shell_profile=".zshrc" ;;
+	*) shell_profile=".bash_profile" ;;
+	esac
+	echo "Manually add the directory to your \$HOME/$shell_profile (or similar)"
+	echo "  export DENO_INSTALL=\"$deno_install\""
+	echo "  export PATH=\"\$DENO_INSTALL/bin:\$PATH\""
+	echo "Run '$exe --help' to get started"
+fi
