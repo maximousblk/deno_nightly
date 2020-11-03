1d5236c1bb8fee65160c9ac29ecf58102d3f39e6
Maximous Black <maximousblk@gmail.com>
2020-11-04 01:46:39 +0530

    don't inject commit in version
    see denoland/deno#8133

--- .github/inject.ts
+++ .github/inject.ts
@@ -1,12 +1,11 @@
 import { parse, Args } from "https://deno.land/std@0.69.0/flags/mod.ts";
 const args: Args = parse(Deno.args);
 const build: string = args.b;
-const commit: string = args.h;
 
-console.log({ build, commit })
+console.log({ build })
 
 const upgrade = await Deno.readTextFile("cli/upgrade.rs");
 await Deno.writeTextFile("cli/upgrade.rs", upgrade.replace(`crate::version::DENO`, `"0.0.1"`));
 
 const data = await Deno.readTextFile("cli/version.rs");
-await Deno.writeTextFile("cli/version.rs", data.replace(`env!("CARGO_PKG_VERSION")`, `"nightly ${build} (${commit.substring(0, 7)})"`));
+await Deno.writeTextFile("cli/version.rs", data.replace(`env!("CARGO_PKG_VERSION")`, `"nightly ${build}"`));
